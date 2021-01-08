239071110a6d36598944a3adda497bd6aea24a4f
sumana2001 <sumana.basu2001@gmail.com>
2021-01-08 14:35:26 +0530

    done

--- app.js
+++ app.js
@@ -17,5 +17,48 @@ client.connect(function(err){
 
     const db = client.db(dbName);
 
-    client.close();
+    findDocuments(db, function(){
+        client.close();
+    })
 });
+
+const insertDocuments =function(db, callback){
+    //Get the documents collection
+    const collection = db.collection('fruits');
+    //Insert some documents
+    collection.insertMany([
+        {
+            name : "Apple",
+            score : 8,
+            review : "Great fruit"
+        },
+        {
+            name : "Orange",
+            score : 6,
+            review : "Kinda sour"
+        },
+        {
+            name : "Banana",
+            score : 9,
+            review : "Great stuff!"
+        }
+    ],function(err,result){
+        assert.strictEqual(err,null);
+        assert.strictEqual(3,result.result.n);
+        assert.strictEqual(3,result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+};
+
+const findDocuments = function(db, callback){
+    //Get the documents collection
+    const collection = db.collection('fruits');
+    //Find some documents
+    collection.find({}).toArray(function(err,docs){
+        assert.strictEqual(err,null);
+        console.log("Found the following records");
+        console.log(docs);
+        callback(docs);
+    })
+}
