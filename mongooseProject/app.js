44b6ba31c2120bf889bbd32370fc73e1fa5fbe13
sumana2001 <sumana.basu2001@gmail.com>
2021-01-08 20:21:03 +0530

    read

--- mongooseProject/app.js
+++ mongooseProject/app.js
@@ -50,24 +50,24 @@ const banana = new Fruit({
     review: "Weird"
 });
 
-Fruit.insertMany([kiwi,orange,banana],function(err){
+/* Fruit.insertMany([kiwi,orange,banana],function(err){
     if(err){
         console.log("error")
     }
     else{
         console.log("Succesful")
     }
-})
+}) */
 
+Fruit.find(function(err, fruits){
+    if(err){
+        console.log(err);
+    }
+    else{
+        mongoose.connection.close();
+        fruits.forEach(function(fruit){
+            console.log(fruit.name);
 
-const findDocuments = function(db, callback){
-    //Get the documents collection
-    const collection = db.collection('fruits');
-    //Find some documents
-    collection.find({}).toArray(function(err,docs){
-        assert.strictEqual(err,null);
-        console.log("Found the following records");
-        console.log(docs);
-        callback(docs);
-    })
-}
+        })
+    }
+});
